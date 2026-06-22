54a7a3e1e150b83f6c38edb4f6a28cdd211aebe8
siedlarczyk-98 <ruben.nogueira@cursosactive.com.br>
2026-06-22 13:31:27 -0300

    Update cell range for writing to Google Sheets

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
             spreadsheet_id=SPREADSHEET_ID,
             json_path=JSON_CREDS,
             aba_nome="Métricas Novas",  
-            celula="S20",
+            celula="S20:T20",
             texto=mensagem_log
         )
     else:
